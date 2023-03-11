d78de263e72b16536b6e00c70beba3e0a23bde76
Timurc1k <MY_NAME@example.com>
2023-03-11 11:34:47 +0500

    add files

---
+++ main.js
@@ -0,0 +1,12 @@
+function ShowTime() {
+    let date = new Date;
+let h1 = document.querySelector('h1');
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();
+h = (h < 10) ? "0" + h : h;
+m = (m < 10) ? "0" + m : m;
+s = (s < 10) ? "0" + s : s;
+h1.innerHTML = `${h} : ${m} : ${s}`;
+}
+setInterval(ShowTime, 1000);
